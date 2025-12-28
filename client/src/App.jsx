1412c90ed5ad3f8c50ec3897dcafd785e34e8236
Pablo Basso <pablobasso25@gmail.com>
2025-12-28 18:13:10 -0300

    feat(frontend): add AuthProvider and useAuth hook for registration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router";
+import { AuthProvider } from "./context/AuthContext.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<h1>Home page</h1>} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/tasks" element={<h1>Tasks page</h1>} />
-        <Route path="/add-task" element={<h1>New task</h1>} />
-        <Route path="/tasks/:id" element={<h1>Update task</h1>} />
-        <Route path="/profile" element={<h1>HProfile</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Home page</h1>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/tasks" element={<h1>Tasks page</h1>} />
+          <Route path="/add-task" element={<h1>New task</h1>} />
+          <Route path="/tasks/:id" element={<h1>Update task</h1>} />
+          <Route path="/profile" element={<h1>HProfile</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 };
 
