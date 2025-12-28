70d2d3c84015c6f357bf35bc8dce9e524002c84f
Pablo Basso <pablobasso25@gmail.com>
2025-12-28 13:04:24 -0300

    feat(frontend): setup routes and skeleton pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-4xl font-bold">Hola mundo</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home page</h1>} />
+        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/register" element={<h1>Register</h1>} />
+        <Route path="/tasks" element={<h1>Tasks page</h1>} />
+        <Route path="/add-task" element={<h1>New task</h1>} />
+        <Route path="/tasks/:id" element={<h1>Update task</h1>} />
+        <Route path="/profile" element={<h1>HProfile</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
