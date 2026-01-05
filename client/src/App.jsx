c1210f3118687245c960ab8582576c2f074f8c18
Pablo Basso <pablobasso25@gmail.com>
2026-01-04 22:45:14 -0300

    feat(pages): add auth and task management pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,23 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
+import TasksPage from "./pages/TasksPage.jsx";
+import TaskFormPage from "./pages/TaskFormPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import HomePage from "./pages/HomePage.jsx";
 
 const App = () => {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Home page</h1>} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/tasks" element={<h1>Tasks page</h1>} />
-          <Route path="/add-task" element={<h1>New task</h1>} />
-          <Route path="/tasks/:id" element={<h1>Update task</h1>} />
-          <Route path="/profile" element={<h1>HProfile</h1>} />
+          <Route path="/tasks" element={<TasksPage />} />
+          <Route path="/add-task" element={<TaskFormPage />} />
+          <Route path="/tasks/:id" element={<TaskFormPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
