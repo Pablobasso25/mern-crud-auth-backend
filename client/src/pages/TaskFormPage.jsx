abbc48d2492c2968530cc90986acbb9b6e20be0c
Pablo Basso <pablobasso25@gmail.com>
2026-01-06 22:38:43 -0300

    feat: implement user authentication and task pages
    - Add login and registration pages with form validation.
    - Implement authentication context to manage user state.
    - Create pages for home, tasks list, task form, and user profile.
    - Set up routing for the new pages.

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,5 +1,32 @@
+import { useForm } from "react-hook-form";
+
 const TaskFormPage = () => {
-  return <div>TaskFormPage</div>;
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+
+  return (
+    <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Título"
+          {...register("título")}
+          autoFocus
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+        />
+        <textarea
+          rows={3}
+          placeholder="Descripción"
+          {...register("descripción")}
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+        ></textarea>
+        <button>Guardar</button>
+      </form>
+    </div>
+  );
 };
 
 export default TaskFormPage;
--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,4 +1,9 @@
+import { useAuth } from "../context/AuthContext";
+
 const TasksPage = () => {
+  const { user } = useAuth();
+  console.log(user);
+
   return <div>TasksPage</div>;
 };
 
