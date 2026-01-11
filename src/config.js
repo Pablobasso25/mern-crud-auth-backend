1ac1ebae174d33f0234b5ffefc436f078c0f9e69
Pablo Basso <pablobasso25@gmail.com>
2026-01-11 00:41:41 -0300

    fix: force dotenv load in config

--- src/config.js
+++ src/config.js
@@ -1,22 +1,8 @@
-/* // Exporto la variable PORT que obtiene su valor de process.env.PORT (del archivo .env)
-// Si no existe en .env, usa el valor por defecto 4002 (temporal para liberar 4000)
-// Esta variable se importa en index.js para definir en qué puerto corre el servidor
-export const PORT = process.env.PORT || 4002;
-
-// Exporto la variable MONGODB_URI que obtiene su valor de process.env.MONGODB_URI (del archivo .env)
-// Contiene la URL de conexión a MongoDB Atlas
-// Si no existe en .env, usa una conexión local por defecto: mongodb://localhost/merndb
-// Esta variable se importa en db.js para conectarse a la base de datos
-export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/merndb";
-
-export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret123"; */
-// config.js
 import { config } from "dotenv";
 config(); // Esto asegura que las variables se carguen ANTES de exportarlas
-
+// Exporto la variable PORT que obtiene su valor de process.env.PORT (del archivo .env)
+// Si no existe en .env, usa el valor por defecto 4006 (temporal para liberar 4000)
+// Esta variable se importa en index.js para definir en qué puerto corre el servidor
 export const PORT = process.env.PORT || 4006;
-export const MONGODB_URI =
-  process.env.MONGODB_URI ||
-  "mongodb+srv://pablobasso25_db:Pablobasso25@pablomongodb.guo2nmn.mongodb.net/merndb?retryWrites=true&w=majority";
-export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret123";
+export const MONGODB_URI = process.env.MONGODB_URI;
+export const TOKEN_SECRET = process.env.TOKEN_SECRET;
--- src/db.js
+++ src/db.js
@@ -5,28 +5,7 @@ import { MONGODB_URI } from "./config.js";
 
 // Esta función asíncrona se encarga de conectar la aplicación a la base de datos MongoDB
 // Se exporta para ser llamada desde index.js cuando inicia la aplicación
-/* export const connectDB = async () => {
-  try {
-    // mongoose.connect() intenta conectarse a MongoDB usando la URI de Atlas
-    // await espera a que la conexión se complete antes de continuar
-    await mongoose.connect(MONGODB_URI);
-    //si todo sale bien le digo que me muestre un mensaje de que la conxión fue exitosa
-    console.log(">>> DB is connected");
-  } catch (error) {
-    // Si hay un error en la conexión (ej: URL incorrecta, internet caído), lo muestra
-    console.log(error);
-  }
-}; */
-/* export const connectDB = async () => {
-  try {
-    // Usamos process.env directamente para asegurar que tome la de Render
-    const uri = process.env.MONGODB_URI || MONGODB_URI;
-    await mongoose.connect(uri);
-    console.log(">>> DB is connected");
-  } catch (error) {
-    console.log("Error de conexión a la DB:", error);
-  }
-}; */
+
 export const connectDB = async () => {
   try {
     // Verificamos que la URI exista antes de intentar conectar
