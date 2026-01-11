fc3468ba81d3342e866f3cf60e9dd3e7f8b345cd
Pablo Basso <pablobasso25@gmail.com>
2026-01-10 22:22:27 -0300

    feat: configure environment variables and CORS for production

--- src/config.js
+++ src/config.js
@@ -10,4 +10,4 @@ export const PORT = process.env.PORT || 4002;
 export const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/merndb";
 
-export const TOKEN_SECRET = process.env.TOKEN_SECRET;
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret123";
